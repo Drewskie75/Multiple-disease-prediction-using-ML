768a26b28d8efc21a7442a993386e891f91299f7
Saurabh Mishra <111102879+Drewskie75@users.noreply.github.com>
2024-01-16 21:28:43 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,7 @@
+L = [1, (2, 3), [4, 5, (6, 7, [8, 9, 10], 11, 12), 13], (14, "Hello", [15, "Python", "HTML"])]
+
+# Access the string "Python" using list slicing
+result = L[3][2]
+
+# Print the result
+print(result)
